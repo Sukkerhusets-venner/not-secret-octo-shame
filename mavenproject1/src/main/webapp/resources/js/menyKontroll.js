eefd859c5cf9d9f40cbea62821e8f3d31aaed556
jonassss <joooog@trl054070.ra.hist.no>
2015-01-13 12:42:20 +0100

    meep

---
+++ mavenproject1/src/main/webapp/resources/js/menyKontroll.js
@@ -0,0 +1,109 @@
+/* 
+ * Bruker jquery 2.1.*
+ */
+
+var REFRESH_TIMER = 3000;
+
+$(document).ready(function () {
+    var a = $("#spill");
+    var b = $("#hiscore");
+    var c = $("#profil");
+    a.css("visibility", "visible");
+
+
+    function showHiScore() {
+        if (a.css("visibility").toString() === ("visible")) {
+            a.fadeTo(500, 0, function () {
+                a.css("visibility", "hidden");
+                b.css("visibility", "visible");
+                b.fadeTo(500, 1);
+            });
+        } else if (c.css("visibility").toString() === ("visible")) {
+            c.fadeTo(500, 0, function () {
+                c.css("visibility", "hidden");
+                b.css("visibility", "visible");
+                b.fadeTo(500, 1);
+            });
+        }
+    }
+    function showSpill() {
+        if (c.css("visibility").toString() === ("visible")) {
+            c.fadeTo(500, 0, function () {
+                c.css("visibility", "hidden");
+                a.css("visibility", "visible");
+                a.fadeTo(500, 1);
+            });
+        } else if (b.css("visibility").toString() === ("visible")) {
+            b.fadeTo(500, 0, function () {
+                b.css("visibility", "hidden");
+                a.css("visibility", "visible");
+                a.fadeTo(500, 1);
+            });
+        }
+    }
+    function showProfil() {
+        if (a.css("visibility").toString() === ("visible")) {
+            a.fadeTo(500, 0, function () {
+                a.css("visibility", "hidden");
+                c.css("visibility", "visible");
+                c.fadeTo(500, 1);
+            });
+        } else if (b.css("visibility").toString() === ("visible")) {
+            b.fadeTo(500, 0, function () {
+                b.css("visibility", "hidden");
+                c.css("visibility", "visible");
+                c.fadeTo(500, 1);
+            });
+        }
+    }
+    function quickFix() { // kjapp test/bugfix
+        var abool = (a.css("visibility").toString() === ("visible"));
+        var bbool = (b.css("visibility").toString() === ("visible"));
+        var cbool = (c.css("visibility").toString() === ("visible"));
+        if (abool && bbool) {
+            a.css("visibility", "hidden");
+            b.css("visibility", "hidden");
+        } else if (abool && cbool) {
+            a.css("visibility", "hidden");
+            c.css("visibility", "hidden");
+        } else if (bbool && cbool) {
+            b.css("visibility", "hidden");
+            c.css("visibility", "hidden");
+        }
+        a.css("visibility", "visible");
+    }
+
+    var divVist = 0;
+    var timer = setInterval(function () {
+        divVist++;
+        if (divVist >= 3) {
+            divVist = 0;
+        }
+        switch (divVist) {
+            case 0:
+                showSpill();
+                break;
+            case 1:
+                showHiScore();
+                break;
+            case 2:
+                showProfil();
+                break;
+        }
+    }, REFRESH_TIMER);
+
+    $("#buttons div a").mouseover(function (event) {
+        var elem = $(event.target).html();
+        clearInterval(timer);
+        if (elem === ("Spillet") && a.css("visibility").toString() === ("hidden")) {
+            showSpill();
+        } else if (elem === ("HiScore") && b.css("visibility").toString() === ("hidden")) {
+            showHiScore();
+        } else if (elem === ("Profil") && c.css("visibility").toString() === ("hidden")) {
+            showProfil();
+        }
+    });
+    $("#buttons div a").mouseout(function () {
+        quickFix();
+    });
+});
