7dbcf1c69b34cc0164b752d8aae56165525be3f5
jonassss <joooog@Jonass-MacBook-Pro.local>
2015-01-15 02:39:19 +0100

    bb

--- mavenproject1/src/main/java/prosjekt/kontroller/loginKontroller.java
+++ mavenproject1/src/main/java/prosjekt/kontroller/loginKontroller.java
@@ -9,12 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-<<<<<<< HEAD
-=======
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import prosjekt.Domene.User;
->>>>>>> FETCH_HEAD
 import prosjekt.Domene.UserScore;
 import prosjekt.Ui.Loginform;
 import prosjekt.database.DatabaseConnection;
@@ -26,19 +22,10 @@ public class loginKontroller {
     @Autowired
     private DatabaseConnection database;
     
-<<<<<<< HEAD
-=======
-    
-     @ModelAttribute("loginform")
+    @ModelAttribute("loginform")
     public Loginform makeAssignment(){
         return new Loginform();
     }
-    
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String meh(){
-        return "error";
-    }
->>>>>>> FETCH_HEAD
     @RequestMapping(value = "/*")
     public String showForm(@ModelAttribute Loginform loginform){
         return "login";
@@ -50,14 +37,11 @@ public class loginKontroller {
             return "login";
         }else if (database.checkLogin(loginform.getUser().getEmail(), loginform.getUser().getPassword())) {
             HttpSession session = request.getSession();
-<<<<<<< HEAD
-            session.setAttribute("currentUser", database.getUser(loginform.getUser().getEmail()).getUsername());
+
             session.setAttribute("loginform", loginform);
-=======
-            session.setAttribute ("Username", database.getUser(loginform.getUser().getEmail()).getUsername());
             session.setAttribute("currentUser", new User( database.getUser(loginform.getUser().getEmail()).getUsername(), 
                     loginform.getUser().getEmail(), loginform.getUser().getPassword()));
->>>>>>> FETCH_HEAD
+
             ArrayList<UserScore> hiScores = database.getHighScoreList();
             loginform.setHiScore(hiScores);
             return "Hovedside";
